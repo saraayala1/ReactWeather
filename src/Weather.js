9324c8696ace91ea465b798766721834d5976ba6
saraayala1 <70584322+saraayala1@users.noreply.github.com>
2020-11-25 17:44:14 -0500

    deleted extra usestate

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
-import React, {useState} from "react";
+import React from "react";
 import axios from "axios";
-import { useState } from "react";
 import './App.css'
 
 export default function Weather(){
@@ -9,11 +8,8 @@ function handleResponse(response){
     console.log(response.data.main.temp);
 }
 
-
     let url="https://api.openweathermap.org/data/2.5/weather?q=London&appid=d65010f0ee255fc171c7d8183e8bf68a&units=metric"
     axios.get(url).then(handleResponse)
-   
-   
    
     return (
         <div>
