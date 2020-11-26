10280737e1bbc1cb9b016c16f1df090704572b6c
saraayala1 <70584322+saraayala1@users.noreply.github.com>
2020-11-25 19:52:46 -0500

    added response

--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import './App.css'
 
 export default function Weather(){
-
+let [city, setCity]= useState(" ");
+let [weather, setWeather]= useState(" ");
 function handleResponse(response){
-    console.log(response.data);
+    setWeather=({
+    temperature= (response.data.main.temp),
+    feelsLike= (response.data.main.feels_like),
+    humidity= (response.data.main.humidity),
+    tempMax= (response.data.main.temp_max),
+    tempMin= (response.data.main.temp_min),
+    description= (response.data.weather[0].description),
+    icon= `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    wind= (response.data.wind.speed)
+    });
 }
-
-    let url="https://api.openweathermap.org/data/2.5/weather?q=London&appid=d65010f0ee255fc171c7d8183e8bf68a&units=metric"
+function handleSubmit(event){
+    event.preventDefault();
+      let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d65010f0ee255fc171c7d8183e8bf68a&units=metric`
     axios.get(url).then(handleResponse)
-   
-    return (
-        <div>
-           <input type="text" className="searchbar" placeholder="Type city here..." autoFocus="on" />
+}
+ function updateCity(event) {
+    setCity(event.target.value);
+  }
+  
+let form=(
+        <form onSubmit={handleSubmit}>
+           <input type="text"
+            className="searchbar"
+            placeholder="Type city here..."
+            autoFocus="on" 
+            onChange={updateCity}/>
            <input type="submit" className="submit" value="Search"/> 
            <button className="currentLocation">Current Location</button>
-       
-       </div>
+       </form>   
     )
-}
+    return (
+        <div>
+        {form}
+        {weather}
+    </div>)
+} 
