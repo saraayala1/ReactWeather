a26309a5d502d62c8c93f5cbec30d64e71d92738
saraayala1 <70584322+saraayala1@users.noreply.github.com>
2020-12-01 11:24:21 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Forecast from "./Forecast"
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <div className="appBox">
      <h1>Weather Forecast</h1>
       <Weather />
@@ -14,7 +15,12 @@ function App() {
       <Forecast day="Thursday" icon="RAIN"/>
       <Forecast day="Friday"icon="SLEET"/>
       <Forecast day="Saturday"icon="FOG"/>
-   </div>
+      </div>
+      <footer> This site was coded by {" "}
+        <a href="https://www.saraswebsites.com" target="_blank"  rel="noopener noreferrer">Sara Ayala </a> 
+       <a href="https://github.com/saraayala1/ReactWeather" target="_blank"  rel="noopener noreferrer"> open sourced</a> on Github
+      </footer>
+    </div>
     </div>
   );
 }
